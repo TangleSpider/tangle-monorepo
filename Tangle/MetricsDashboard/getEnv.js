aeebf6aabae8fa3ccb1401e58dfdd1d07892325a
Brad Leishman <spider@tangle.co.in>
2021-09-16 03:35:48 -0400

    Revert "add Metrics Dashboard Backend"
    This reverts commit 58d39fe42c355ba2f8c29b196be6c1a3ddec7565.

--- Tangle/MetricsDashboard/getEnv.js
+++
@@ -1,9 +0,0 @@
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
-require("dotenv").config();
-
-let getEnv = envVar => {
-    return process.env[envVar];
-};
-
-export { getEnv };
