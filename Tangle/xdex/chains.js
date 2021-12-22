05a3bad05343afa28c71a8196acdcfb8fbd70a55
TangleSpider <spider@tangle.co.in>
2021-12-22 15:36:28 -0500

    chains.js fix, geth/init updates, added geth/lib, unitTest rename and updates, xdex rename and updates. general streamlining of testing process. all testing now done with one command (geth/init)

--- Tangle/xdex/chains.js
+++ Tangle/xdex/chains.js
@@ -1,7 +1,7 @@
 let ethers = require("ethers");
 let fs = require("fs");
-let logs14 = fs.readFileSync("geth/14/logs", "utf8");
-let logs15 = fs.readFileSync("geth/15/logs", "utf8");
+let logs14 = fs.readFileSync("/code/XDEX/geth/14/logs", "utf8");
+let logs15 = fs.readFileSync("/code/XDEX/geth/15/logs", "utf8");
 let trcAddress14 = logs14.match(/trcaddress:([\d\w]+)/)[1];
 let trcAddress15 = logs15.match(/trcaddress:([\d\w]+)/)[1];
 
@@ -25,29 +25,28 @@ let chains = {
         TangleRelayerContract: trcAddress14,
         TangleRelayer: "0x685d9b9b070a5e79c53FC3d511cbBf46585Ef978",
         rpcUrl: "http://127.0.0.1:8014/",
-        filterChunkSize: 0
+        filterChunkSize: 5000
     },
     ["0x" + "f".padStart(64, '0')]: { // + P15
         name: "P15",
         TangleRelayerContract: trcAddress15,
         TangleRelayer: "0x685d9b9b070a5e79c53FC3d511cbBf46585Ef978",
         rpcUrl: "http://127.0.0.1:8015/",
-        filterChunkSize: 0
+        filterChunkSize: 1
     }
 };
 
 Object.entries(chains).map(async chain => {
-    chain[1].shortId = chain[0].replace(/0+(?!x|$)/, "");
-    chain[1].longId = chain[0];
-    chain[1].filters = {
+    chains[chain[0]].shortId = chain[0].replace(/0+(?!x|$)/, "");
+    chains[chain[0]].longId = chain[0];
+    chains[chain[0]].filters = {
         paymentReceived: {
-            address: chain[1].TangleRelayerContract,
+            address: chains[chain[0]].TangleRelayerContract,
             topics: [ethers.utils.id(
                 "PaymentReceived(uint256,uint256,uint256)"
             )]
         }
     };
-    chain[1].provider = new ethers.getDefaultProvider(chain[1].rpcUrl);
-    chains[chain[0]] = chain[1];
+    chains[chain[0]].provider = new ethers.getDefaultProvider(chains[chain[0]].rpcUrl);
     module.exports = chains;
 });
