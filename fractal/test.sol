b0eb13c1f576729dd49ca94f1ae6f9d41eabe626
TangleSpider <spider@tngl.io>
2022-02-10 04:43:04 -0500

    remove deploy from fractal/, move fractal.sol to fractal/, remove test.sol

--- fractal/test.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.11;
-
-contract HelloWorld {
-    function helloWorld() external pure returns (string memory) {
-        return "Hello world!";
-    }
-}
